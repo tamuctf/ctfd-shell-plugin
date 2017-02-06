9fcc48bc682567897f0304f6d4e6cf8a1bd17357
Andrew Meserole <ameserole@gmail.com>
2017-02-06 10:16:02 -0600

    Update SSH dockerfile and setup script

--- server-scripts/setup.sh
+++ server-scripts/setup.sh
@@ -12,8 +12,13 @@ chmod +x user-shell
 cp user-shell /usr/local/bin/user-shell
 sh -c "echo '/usr/local/bin/user-shell' >> /etc/shells"
 
-pushd ../docker
+pushd ../docker/user-docker
     docker build -t user-image . -m 500M 
 popd
 
-
+pushd ../docker/ssh-docker
+	docker build -t shell-image .
+	docker create -it --name shell-server shell-image
+	docker start shell-servershell-server
+	docker exec shell-server /usr/sbin/sshd -p 31337
+popd
