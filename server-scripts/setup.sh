ea1294bb728022d3a7c6593de603b7b316fe9e65
Andrew Meserole <ameserole@gmail.com>
2017-02-06 13:36:08 -0600

    Update Dockerfile and Setup Script

--- server-scripts/setup.sh
+++ server-scripts/setup.sh
@@ -18,7 +18,8 @@ popd
 
 pushd ../docker/ssh-docker
 	docker build -t shell-image .
-	docker create -it --name shell-server shell-image
-	docker start shell-servershell-server
-	docker exec shell-server /usr/sbin/sshd -p 31337
+	docker create -it --name shell-server -p 4200:4200 -p 31337:31337 shell-image
+	docker start shell-server
+	docker exec -d shell-server shellinaboxd -b -t -p 4200 &
+	docker exec -d shell-server /usr/sbin/sshd -p 31337 &
 popd
