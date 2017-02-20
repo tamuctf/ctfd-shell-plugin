52d89f6ba9fde72052d250c53a23cf6d89379fa7
Andrew Meserole <ameserole@gmail.com>
2017-02-19 22:55:28 -0600

    Update Dockerfile and Setup Script
    This commit updates the Dockerfile and setup script to allow the
    automatic creation of a docker image that allows commands to be
    run from the container onto the host image.

--- server-scripts/setup.sh
+++ server-scripts/setup.sh
@@ -18,7 +18,7 @@ popd
 
 pushd ../docker/ssh-docker
 	docker build -t shell-image .
-	docker create -it --name shell-server -p 4200:4200 -p 31337:31337 shell-image
+	docker create -it --name shell-server -p 4200:4200 -p 31337:31337 -v /var/run/docker.sock:/var/run/docker.sock shell-image
 	docker start shell-server
 	docker exec -d shell-server shellinaboxd -b -t -p 4200 &
 	docker exec -d shell-server /usr/sbin/sshd -p 31337 &
