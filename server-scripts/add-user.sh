b7660cbcc9f59b39e50c51ecc2107ac9d8e29771
Andrew Meserole <ameserole@gmail.com>
2017-02-22 12:23:04 -0600

    Automatically Set Hostname of Containers to tamuctf-shell

--- server-scripts/add-user.sh
+++ server-scripts/add-user.sh
@@ -5,9 +5,8 @@ echo -e "$2\n$2" | passwd "$1"
 
 chsh -s /usr/local/bin/user-shell "$1"
 
-docker create -it -w "/home/$1" --name "$1" user-image /bin/bash
+docker create -it -w "/home/$1" --name "$1" -h tamuctf-shell user-image /bin/bash
 
 docker start "$1"
 docker exec "$1" useradd -ms /bin/bash "$1"
 docker stop "$1"
-
--- server-scripts/setup.sh
+++ server-scripts/setup.sh
@@ -4,8 +4,8 @@ apt-get update
 
 apt-get install -y docker
 
-cp add-user.sh ../../docker/ssh-docker/
-cp user-shell ../../docker/ssh-docker/
+cp add-user.sh ../docker/ssh-docker/
+cp user-shell ../docker/ssh-docker/
 
 pushd ../docker/user-docker
     docker build -t user-image . -m 500M 
@@ -13,7 +13,7 @@ popd
 
 pushd ../docker/ssh-docker
 	docker build -t shell-image .
-	docker create -it --name shell-server -p 4200:4200 -p 31337:31337 -v /var/run/docker.sock:/var/run/docker.sock shell-image
+	docker create -it --name shell-server -h tamuctf-shell -p 4200:4200 -p 31337:31337 -v /var/run/docker.sock:/var/run/docker.sock shell-image
 	docker start shell-server
 	docker exec -d shell-server shellinaboxd -b -t -p 4200 &
 	docker exec -d shell-server /usr/sbin/sshd -p 31337 &
