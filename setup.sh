437f611d52bc3e20193fbd7a2b7bce5b5dd88d37
Andrew Meserole <ameserole@gmail.com>
2017-04-01 15:28:21 -0500

    Update to work with shell server

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 
-apt-get install -y docker
+apt-get install -y docker.io python-pip
 
 cp server-scripts/add-user.sh docker/ssh-docker/
 cp server-scripts/user-shell docker/ssh-docker/
@@ -16,7 +16,7 @@ popd
 
 pushd docker/ssh-docker
 	docker build -t shell-image .
-	docker create -it --name shell-server -h tamuctf-shell -p 4200:4200 -p 31337:31337 -v /var/run/docker.sock:/var/run/docker.sock shell-image
+	docker create -it --name shell-server -h tamuctf-shell -p 4200:4200 -p 2222:2222 -v /var/run/docker.sock:/var/run/docker.sock shell-image
 	docker start shell-server
 	docker exec -d shell-server shellinaboxd -b -t -p 4200 &
 	docker exec -d shell-server /usr/sbin/sshd -p 2222 &
