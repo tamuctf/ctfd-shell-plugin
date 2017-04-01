bf6dbbe056d49ffe0373ad70bf362e608378c376
Andrew Meserole <ameserole@gmail.com>
2017-04-01 13:58:21 -0500

    Add xmlrpc Server Script

---
+++ server-scripts/script_server.py
@@ -0,0 +1,30 @@
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+import os
+
+#https://docs.python.org/2/library/simplexmlrpcserver.html
+
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+server = SimpleXMLRPCServer(("localhost", 8000),
+                            requestHandler=RequestHandler,
+			    allow_none=True)
+
+server.register_introspection_functions()
+
+def hello_func(name):
+        return "Hello, {}!".format(name)
+
+def add_user_func(name, password):
+	os.system("docker exec shell-server ./add-user.sh " + name + " " + password)
+	
+def change_user_func(name, password):
+	os.system("docker exec shell-server ./change-user-pass.sh " + name + " " + password)
+
+server.register_function(hello_func, 'hello')
+server.register_function(add_user_func, 'add_user')
+server.register_function(change_user_func, 'change_user')
+
+server.serve_forever()
+
