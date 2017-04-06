5d31d14dabded57b133ce6acd784ed46e4db7344
Andrew Meserole <ameserole@gmail.com>
2017-04-06 12:26:09 -0500

    Better user management

--- server-scripts/add-user.sh
+++ server-scripts/add-user.sh
@@ -6,8 +6,10 @@ echo -e "$2\n$2" | passwd "$1"
 
 chsh -s /usr/local/bin/user-shell "$1"
 
-docker create -it --name "$1" -w /home -e TMOUT=300 -h tamuctf-shell --cpus=".5" --memory="500M" user-image /bin/bash
+docker build -t user-image --build-arg USER="$1" -f docker/user-docker/Dockerfile github.com/tamuctf/CTFd-shell-plugin 
 
-docker start "$1"
-docker exec "$1" useradd -s /bin/bash "$1"
-docker stop "$1"
+docker create -it --name "$1" -w /home/"$1" --read-only -e TMOUT=300 -h tamuctf-shell --cpus=".5" --memory="500M" user-image /bin/bash
+
+#docker start "$1"
+#docker exec "$1" useradd -ms /bin/bash "$1"
+#docker stop "$1"
--- setup.sh
+++ setup.sh
@@ -2,21 +2,21 @@
 
 set -euo pipefail
 
-apt-get update
+sudo apt-get update
 
-apt-get install -y docker.io python-pip
+sudo apt-get install -y docker.io python-pip
 
 cp server-scripts/add-user.sh docker/ssh-docker/
 cp server-scripts/user-shell docker/ssh-docker/
 cp server-scripts/change-user-pass.sh docker/ssh-docker
 
-pushd docker/user-docker
-    docker build -t user-image . -m 500M 
-popd
+#pushd docker/user-docker
+#    docker build -t user-image . -m 500M 
+#popd
 
 pushd docker/ssh-docker
 	docker build -t shell-image .
-	docker create -it --name shell-server -h tamuctf-shell -p 4200:4200 -p 2222:2222 -v /var/run/docker.sock:/var/run/docker.sock shell-image
+	docker create -it --name shell-server -h tamuctf-shell -p 4200:4200 -p 2222:2222 -v /dev/fuse:/dev/fuse -v /var/run/docker.sock:/var/run/docker.sock shell-image
 	docker start shell-server
 	docker exec -d shell-server shellinaboxd -b -t -p 4200 &
 	docker exec -d shell-server /usr/sbin/sshd -p 2222 &
